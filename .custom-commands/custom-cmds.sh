8071b9c94d2278d483a21f2a586b25ef31f467a5
Miku <masami45@tuta.io>
2021-01-20 15:31:56 +0700

    (Custom Commands) fixing ugly indents

--- .custom-commands/custom-cmds.sh
+++ .custom-commands/custom-cmds.sh
@@ -5,41 +5,39 @@
 # Audio - {251} webm 48K
 # Use `youtube-dl -F <VIDEO_URL>` to get available format code list
 function yt-dl() {
-	youtube-dl -f '135' $1 && youtube-dl -f '251' $1
+  youtube-dl -f '135' $1 && youtube-dl -f '251' $1
 }
 
 # Combine ytdl downloaded file to MKV
 function make-mkv() {
-	mkvmerge --ui-language en_US --output "$1.mkv" --language 0:und '(' "$1.mp4" ')' --language 0:eng --default-track 0:yes '(' "$1.webm" ')' --track-order 0:0,1:0
+  mkvmerge --ui-language en_US --output "$1.mkv" --language 0:und '(' "$1.mp4" ')' --language 0:eng --default-track 0:yes '(' "$1.webm" ')' --track-order 0:0,1:0
 }
 
 # Convert mkv file to mp4 using ffmpeg
 function mkv-to-mp4() {
-	ffmpeg -i "$1.mkv" -c:v copy "$1.mp4"
+  ffmpeg -i "$1.mkv" -c:v copy "$1.mp4"
 }
 
 # Delete downloaded file after make-mkv is success
 function del-file() {
-    rm "$1.mp4" && rm "$1.webm"
+  rm "$1.mp4" && rm "$1.webm"
 }
 
 # Combine all command above
 function ytdl() {
-	local videoName=$(youtube-dl --get-filename $1 | sed 's/\.[^.]*$//')
-
-	yt-dl $1 && make-mkv "$videoName" && del-file "$videoName" && mkv-to-mp4 "$videoName" && rm "$videoName.mkv"
+  local videoName=$(youtube-dl --get-filename $1 | sed 's/\.[^.]*$//')
+  yt-dl $1 && make-mkv "$videoName" && del-file "$videoName" && mkv-to-mp4 "$videoName" && rm "$videoName.mkv"
 }
 
 # youtube-dl 360p
 function ytdl-360() {
-    local videoName=$(youtube-dl --get-filename $1 | sed 's/\.[^.]*$//')
-
-	youtube-dl -f '18' $1 && youtube-dl -f '251' $1 && make-mkv "$videoName" && del-file "$videoName" && mkv-to-mp4 "$videoName" && rm "$videoName.mkv"
+  local videoName=$(youtube-dl --get-filename $1 | sed 's/\.[^.]*$//')
+  youtube-dl -f '18' $1 && youtube-dl -f '251' $1 && make-mkv "$videoName" && del-file "$videoName" && mkv-to-mp4 "$videoName" && rm "$videoName.mkv"
 }
 
 # Make multiple CBZ file
 function make-cbz() {
-	zip -rj "$1.cbz" "$1"
+  zip -rj "$1.cbz" "$1"
 }
 
 # Starting httpd & mysqld
