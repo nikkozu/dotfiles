1c14233bce1af750168f6ed76e597741ad236c70
masami45 <masami45@tuta.io>
2021-06-20 00:15:14 +0700

    refactor: replace ytdl custom commands
    replace old ytdl script with new one written in python file

--- .custom-commands/custom-cmds.sh
+++ .custom-commands/custom-cmds.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 
-# Shortcut command to use youtube-dl
-# Video - {135} mp4 480p
-# Audio - {251} webm 48K
-# Use `youtube-dl -F <VIDEO_URL>` to get available format code list
-function yt-dl() {
-  local videoQuality=135
-  local audioQuality=251
-  youtube-dl -f "$videoQuality" $1 && youtube-dl -f "$audioQuality" $1
-}
-
-# Combine ytdl downloaded file to MKV
-function make-mkv() {
-  mkvmerge --ui-language en_US --output "$1.mkv" --language 0:und '(' "$1.mp4" ')' --language 0:eng --default-track 0:yes '(' "$1.webm" ')' --track-order 0:0,1:0
-}
-
-# Convert mkv file to mp4 using ffmpeg
-function mkv-to-mp4() {
-  ffmpeg -i "$1.mkv" -c:v copy "$1.mp4"
-}
-
-# Delete downloaded file after make-mkv is success
-function del-file() {
-  rm "$1.mp4" && rm "$1.webm"
-}
-
-# Combine all command above
-function ytdl() {
-  local videoName=$(youtube-dl --get-filename $1 | sed 's/\.[^.]*$//')
-  yt-dl $1 && make-mkv "$videoName" && del-file "$videoName" && mkv-to-mp4 "$videoName" && rm "$videoName.mkv"
-}
-
-# youtube-dl 360p
-function ytdl-360() {
-  local videoName=$(youtube-dl --get-filename $1 | sed 's/\.[^.]*$//')
-  youtube-dl -f '18' $1 && youtube-dl -f '251' $1 && make-mkv "$videoName" && del-file "$videoName" && mkv-to-mp4 "$videoName" && rm "$videoName.mkv"
-}
-
 # Make multiple CBZ file
 function make-cbz() {
   zip -rj "$1.cbz" "$1"
+}
+
+# restart the swap
+function restart-swap() {
+  sudo swapoff -a && sudo swapon -a
+}
+
+function crandr() {
+  if [[ $# -eq 0 ]] ; then
+    crandr start
+  fi
+
+  for arg in "$@"
+  do
+    case $arg in
+      stop)
+        xrandr --output VGA1 --off
+        shift
+        ;;
+      start)
+        xrandr --newmode "1280x768_60.00"   79.50  1280 1344 1472 1664  768 771 781 798 -hsync +vsync
+        xrandr --addmode VGA1 "1280x768_60.00"
+        xrandr --output LVDS1 --mode 1366x768
+        xrandr --output VGA1 --mode 1280x768_60.00 --right-of LVDS1
+        shift
+        ;;
+    esac
+  done
+}
+
+# Python custom commands
+function get-doujin() {
+  $HOME/.custom-commands/get-doujin
+}
+
+function testing() {
+  $HOME/.custom-commands/testing "$@"
+}
+
+function ytdl() {
+  $HOME/.custom-commands/ytdl "$1"
 }
 
 # Starting httpd & mysqld
@@ -107,56 +111,46 @@ function lemp() {
   done
 }
 
-# restart the swap
-function restart-swap() {
-  sudo swapoff -a && sudo swapon -a
-}
 
-function crandr() {
-  if [[ $# -eq 0 ]] ; then
-    crandr start
-  fi
-
-  for arg in "$@"
-  do
-    case $arg in
-      stop)
-        xrandr --output VGA1 --off
-        shift
-        ;;
-      start)
-        xrandr --newmode "1280x768_60.00"   79.50  1280 1344 1472 1664  768 771 781 798 -hsync +vsync
-        xrandr --addmode VGA1 "1280x768_60.00"
-        xrandr --output LVDS1 --mode 1366x768
-        xrandr --output VGA1 --mode 1280x768_60.00 --right-of LVDS1
-        shift
-        ;;
-    esac
-  done
-}
-
-# Python custom commands
-function get-doujin() {
-  $HOME/.custom-commands/get-doujin
-}
-
-function testing() {
-  $HOME/.custom-commands/testing "$@"
-}
-
-# test command
-# function ytdl-test() {
-  # VIDEO=135
-  # AUDIO=251
-  # while getopts "v:a:" opt; do
-    # case $opt in
-      # v) VIDEO=$OPTARG ;;
-      # a) AUDIO=$OPTARG ;;
-      # *) echo 'error' >&251
-        # exit 135
-    # esac
-  # done
-  # echo "Video: $VIDEO"
-  # echo "Audio: $AUDIO"
-  # echo "Url: ${@: -1}"
+# function bw-add() {
+  # local loginTemplate=$(bw get template item.login | jq ".username=\"$2\" | .password=\"$3\"")
+  # bw get template item | jq ".name=\"$1\" | .login=$loginTemplate" | bw encode | bw create item
 # }
+
+# # Shortcut command to use youtube-dl
+# # Video - {135} mp4 480p
+# # Audio - {251} webm 48K
+# # Use `youtube-dl -F <VIDEO_URL>` to get available format code list
+# function yt-dl() {
+  # local videoQuality=135
+  # local audioQuality=251
+  # youtube-dl -f "$videoQuality" $1 && youtube-dl -f "$audioQuality" $1
+# }
+
+# # Combine ytdl downloaded file to MKV
+# function make-mkv() {
+  # mkvmerge --ui-language en_US --output "$1.mkv" --language 0:und '(' "$1.mp4" ')' --language 0:eng --default-track 0:yes '(' "$1.webm" ')' --track-order 0:0,1:0
+# }
+
+# # Convert mkv file to mp4 using ffmpeg
+# function mkv-to-mp4() {
+  # ffmpeg -i "$1.mkv" -c:v copy "$1.mp4"
+# }
+
+# # Delete downloaded file after make-mkv is success
+# function del-file() {
+  # rm "$1.mp4" && rm "$1.webm"
+# }
+
+# # Combine all command above
+# function ytdl() {
+  # local videoName=$(youtube-dl --get-filename $1 | sed 's/\.[^.]*$//')
+  # yt-dl $1 && make-mkv "$videoName" && del-file "$videoName" && mkv-to-mp4 "$videoName" && rm "$videoName.mkv"
+# }
+
+# # youtube-dl 360p
+# function ytdl-360() {
+  # local videoName=$(youtube-dl --get-filename $1 | sed 's/\.[^.]*$//')
+  # youtube-dl -f '18' $1 && youtube-dl -f '251' $1 && make-mkv "$videoName" && del-file "$videoName" && mkv-to-mp4 "$videoName" && rm "$videoName.mkv"
+# }
+
